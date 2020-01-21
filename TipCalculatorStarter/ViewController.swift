955dcaaf3f8c3515ad63e7d157d6e42e006452c4
nickKearns <nickkearns133@gmail.com>
2020-01-20 23:43:11 -0800

    layout and constraints done

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -9,11 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    @IBOutlet weak var billAmountTextField: BillAmountTextField!
+    
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var outputCardView: UIView!
+    
+    
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    
     // MARK: - View Lifecycle
     
+    
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
+        
+        
     }
+    
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch is toggled on")
+        } else {
+            print("switch is toggled off")
+        }
+        
+    }
+    
+    @IBAction func tipPercentChanged(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
